5157a128f294c3b3f575ae042636b1ccb1868264
joeygaitan <joeygaitan707@gmail.com>
2020-04-13 04:58:39 -0700

    fixed up messengerboard

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def home(request):
+    return HttpResponse( "Hello Friend")
+
+def contact(request):
+    return HttpResponse( "Contact Me")
+
+def greet_by_name(request, name):
+    return HttpResponse(f"hello {name}")
